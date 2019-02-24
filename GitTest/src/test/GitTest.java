da8059727d66bfc67d8510b58405bfaf0f2ef3c2
李淑恒 <lishuheng12345@163.com>
2019-02-24 19:59:26 +0800

    git冲突测试

---
+++ GitTest/src/test/GitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        System.out.println("HelloWorld");
+    }
+
+
+}
